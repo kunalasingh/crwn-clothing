61408655e4929dcd42e7f089cdb07871a6e8c6fd
kunal <kunalaksingh.22@gmail.com>
2021-09-19 18:16:42 +0530

    created pages and components folder and menu-item and directory components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => {
+  return <Directory />;
+};
+
+export default HomePage;
