2342059b36bcf5a4bf114474958b1da06658959b
kunal <kunalaksingh.22@gmail.com>
2021-09-20 18:49:28 +0530

    added header component and styling

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { Link } from "react-router-dom";
+
+import "./header.styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
