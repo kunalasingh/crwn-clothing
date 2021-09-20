99099a16547400c2febb54847b54a8316feafb1e
kunal <kunalaksingh.22@gmail.com>
2021-09-20 13:03:23 +0530

    added collection preview and collection item with styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import { Route, Switch } from "react-router";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const Hats = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-);
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={Hats} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
