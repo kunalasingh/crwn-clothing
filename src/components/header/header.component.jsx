59a0d5214cb8f166b01a223aa0e946a392ea1a10
kunal <kunalaksingh.22@gmail.com>
2021-09-24 11:37:41 +0530

    added cart dropdown component and added state to redux for hidden state in header and cart-icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link to="/">
@@ -34,12 +35,14 @@ const Header = ({ currentUser }) => {
 
         <CartIcon />
       </div>
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
 
 const mapStatetoProps = (state) => ({
   currentUser: state.user.currentUser,
+  hidden: state.cart.hidden,
 });
 
 export default connect(mapStatetoProps)(Header);
