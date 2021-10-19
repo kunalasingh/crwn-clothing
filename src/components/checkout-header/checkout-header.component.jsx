19d08cd9f7d9792d2c1585de9dfa64f002c69202
kunal <kunalaksingh.22@gmail.com>
2021-10-19 12:14:19 +0530

    created checkout-header component and slimmed checkout page component

---
+++ src/components/checkout-header/checkout-header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./checkout-header.styles.scss";
+
+const CheckoutHeader = () => (
+  <div className="checkout-header">
+    <div className="header-block">
+      <span>Product</span>
+    </div>
+    <div className="header-block">
+      <span>Description</span>
+    </div>
+    <div className="header-block">
+      <span>Quantity</span>
+    </div>
+    <div className="header-block">
+      <span>Price</span>
+    </div>
+    <div className="header-block">
+      <span>Remove</span>
+    </div>
+  </div>
+);
+
+export default CheckoutHeader;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,27 +10,12 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import CheckoutHeader from "../../components/checkout-header/checkout-header.component";
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
-        <span>Product</span>
-      </div>
-      <div className="header-block">
-        <span>Description</span>
-      </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span>Price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
-    </div>
+    <CheckoutHeader />
     {cartItems.map((item) => (
       <CheckoutItem key={item.id} cartItem={item} />
     ))}
