26e6dcd27ce918badce0e833b248b37222eae596
kunal <kunalaksingh.22@gmail.com>
2021-10-19 12:33:58 +0530

    modified checkout page component to use useSelector and removed connect

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./checkout.styles.scss";
 
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { useSelector } from "react-redux";
 
 import {
   selectCartItems,
@@ -13,27 +12,27 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import CheckoutHeader from "../../components/checkout-header/checkout-header.component";
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <CheckoutHeader />
-    {cartItems.map((item) => (
-      <CheckoutItem key={item.id} cartItem={item} />
-    ))}
-    <div className="total">
-      <span>Total : ${total}</span>
+const CheckoutPage = () => {
+  const total = useSelector(selectCartTotal);
+  const cartItems = useSelector(selectCartItems);
+  const checkoutItems = cartItems.map((item) => (
+    <CheckoutItem key={item.id} cartItem={item} />
+  ));
+  return (
+    <div className="checkout-page">
+      <CheckoutHeader />
+      {checkoutItems}
+      <div className="total">
+        <span>Total : ${total}</span>
+      </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments
+        <br />
+        4242 4242 4242 4242 - Exp 01/50 - CVV 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
-    <div className="test-warning">
-      *Please use the following test credit card for payments
-      <br />
-      4242 4242 4242 4242 - Exp 01/50 - CVV 123
-    </div>
-    <StripeCheckoutButton price={total} />
-  </div>
-);
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal,
-});
-
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
